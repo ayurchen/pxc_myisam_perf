9e1ab36dacc65a6ccc2c9fd448032c4ca83c45d2
Jay Janssen <jay.janssen@gmail.com>
2013-08-15 09:17:51 -0400

    Added sysbench install to base init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -4,6 +4,7 @@ include percona::server
 include percona::config
 
 include misc
+include misc::sysbench
 
 Class['percona::repository'] -> Class['percona::config'] -> Class['percona::server']
 Class['percona::repository'] -> Class['percona::toolkit']
