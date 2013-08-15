65e67d8d5505c863d2fb523e4313242ae90eb550
Jay Janssen <jay.janssen@gmail.com>
2013-08-15 15:11:43 -0400

    Non-index update

--- test_defs.sh
+++ test_defs.sh
@@ -3,6 +3,7 @@
 PREPARE_MYISAM="sysbench --test=sysbench_tests/db/common.lua --mysql-table-engine=MyISAM --mysql-user=root --mysql-db=test --oltp-table-size=1000000 prepare"
 PREPARE_INNODB="sysbench --test=sysbench_tests/db/common.lua --mysql-table-engine=InnoDB --mysql-user=root --mysql-db=test --oltp-table-size=1000000 prepare"
 
-RUN_TEST="sysbench --test=sysbench_tests/db/update_index.lua --mysql-user=root --mysql-db=test --oltp-table-size=1000000 --max-requests=0 --max-time=600 --num-threads=16 --report-interval=1 run"
+
+RUN_TEST="sysbench --test=sysbench_tests/db/update_non_index.lua --mysql-user=root --mysql-db=test --oltp-table-size=1000000 --max-requests=0 --max-time=120 --num-threads=16 --report-interval=1 run"
 
 CLEANUP="sysbench --test=sysbench_tests/db/common.lua --mysql-user=root --mysql-db=test cleanup"
